14d646ff17625bf5785fb99cbccf1210c42d637e
Lester Jan Artienda <artiele@oocl.com>
2021-02-25 19:41:05 +0800

    refactor constant values that may be used across files

---
+++ src/main/java/com/hicx/solutions/constant/constants.java
@@ -0,0 +1,9 @@
+package com.hicx.solutions.constant;
+
+public class constants {
+  public static final String[] INVALID_CHARACTERS = {"?", "!",",","~","-"};
+  public static final String SEPARATOR_SPACE = " ";
+  public static final String REPLACE_NOTHING = "";
+  public static final String CHARACTER_PATTERN_DOT = ".";
+
+}
