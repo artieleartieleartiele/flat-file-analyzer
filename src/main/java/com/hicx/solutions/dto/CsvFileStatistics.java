cb5ad78a2ebc21c980a25f92719f010851fb4db2
Lester Jan Artienda <artiele@oocl.com>
2021-02-25 18:26:47 +0800

    update ToString

--- src/main/java/com/hicx/solutions/dto/CsvFileStatistics.java
+++ src/main/java/com/hicx/solutions/dto/CsvFileStatistics.java
@@ -1,29 +1,22 @@
 package com.hicx.solutions.dto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.List;
 
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper=true)
 public class CsvFileStatistics extends FileStatistics {
     private int numRows;
 
     public CsvFileStatistics(String filename, int wordCount, int dotCount, Pair<List<String>, Integer> popularWords, int numRows) {
         super(filename, wordCount, dotCount, popularWords);
         this.numRows = numRows;
-    }
-
-    @Override
-    public String toString() {
-        return "CsvFileStatistics{" +
-                "filename='" + super.getFilename() + '\'' +
-                ", wordCount=" + super.getWordCount() +
-                ", dotCount=" + super.getDotCount() +
-                ", popularWords=" + super.getPopularWords() +
-                ", numRows=" + this.getNumRows() +
-                '}';
     }
 }
--- src/main/java/com/hicx/solutions/dto/PlainTextFileStatistics.java
+++ src/main/java/com/hicx/solutions/dto/PlainTextFileStatistics.java
@@ -1,27 +1,16 @@
 package com.hicx.solutions.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.List;
 
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper=true)
 public class PlainTextFileStatistics extends FileStatistics {
     public PlainTextFileStatistics(String filename, int wordCount, int dotCount, Pair<List<String>, Integer> popularWords) {
         super(filename, wordCount, dotCount, popularWords);
-    }
-
-    @Override
-    public String toString() {
-        return "PlainTextFileStatistics{" +
-                "filename='" + super.getFilename() + '\'' +
-                ", wordCount=" + super.getWordCount() +
-                ", dotCount=" + super.getDotCount() +
-                ", popularWords=" + super.getPopularWords() +
-                '}';
     }
 }
